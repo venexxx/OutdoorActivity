f9450bba87b626ef193be4b85b6aa7d90a09d7ee
Zdrave Gocevski <zdravegocevski123@gmail.com>
2026-06-24 23:20:50 +0300

    Spring Project setup

---
+++ src/main/java/org/example/outdooractivity/OutdoorActivityApplication.java
@@ -0,0 +1,13 @@
+package org.example.outdooractivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutdoorActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutdoorActivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/outdooractivity/OutdoorActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.outdooractivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdoorActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
